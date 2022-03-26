2b0a10c5eb06b333bf7ec85d5f9e0a54d4cf503f
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-03-27 00:11:15 +0600

    Main JS General Layout

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== SERVICES MODAL ===============*/
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+/* Link Active Work */
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== LIGHT DARK THEME ===============*/
+
+/*=============== SCROLL REVEAL AIMATION ===============*/
